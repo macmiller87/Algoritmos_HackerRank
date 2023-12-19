41ad4652e561bb862f64884854c1dc0c395e4a2f
macmiller87 <macamagolf@gmail.com>
2023-12-19 16:14:06 -0300

    Feat: resolution problem jumping-on-the-clouds

---
+++ javascript/Jumping_on_the_Clouds/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+    
+    let clouds = c;
+    
+    let jumps = 0;
+    let startJump = 1;
+    
+    let i = 0;
+    
+    while(i < (clouds.length - startJump)) {
+        
+        if(clouds[i + 2] === 0) {
+            jumps++; 
+            i+= 2; 
+            
+        }else {
+            jumps++;
+            i+= 1;
+        }
+    }
+    
+    return jumps;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
